b42cb7e7b6d52ea33edd2c55242ef90fdf815787
MEOWhyerin <engineerhyerin@gmail.com>
2024-10-15 23:24:25 +0900

    sp

---
+++ 10_example_3/10_example_3.ino
@@ -0,0 +1,36 @@
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/faq/how-to-control-speed-of-servo-motor
+ */
+
+#include <Servo.h>
+
+#define PIN_SERVO 10
+
+Servo myServo;
+unsigned long MOVING_TIME = 3000; // moving time is 3 seconds
+unsigned long moveStartTime;
+int startAngle = 0; // 30°
+int stopAngle  = 180; // 90°
+
+void setup() {
+  myServo.attach(PIN_SERVO);
+  moveStartTime = millis(); // start moving
+
+  myServo.write(startAngle); // Set position
+  delay(500);
+}
+
+void loop() {
+  unsigned long progress = millis() - moveStartTime;
+
+  if (progress <= MOVING_TIME) {
+    long angle = map(progress, 0, MOVING_TIME, startAngle, stopAngle);
+    float x = ((float)progress / (float)MOVING_TIME) * 12 - 6; // logistic function과 같이 -6부터 6까지로 범위를 조정함. 입력값이 -6일 때는 0 6일 때는 1에 가까움 
+    float sigmoid = 1 / ( 1 + exp(-x));
+    myServo.write(angle); 
+  }
+}
